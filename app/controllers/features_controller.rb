00aa6b92d468ae7addd11c6f8e625d83187f74c6
Jeremy Vandehey <jgvandehey@gmail.com>
2014-12-15 12:08:06 -0800

    remove group by

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -3,8 +3,8 @@ class FeaturesController < ApplicationController
   # GET /features.json
   def index
     # @features = Feature.all
-    # @features = Feature.find_with_reputation(:votes, :all, order: 'votes desc')
-    @features = Feature.group_by_day(:created_at).count
+    @features = Feature.find_with_reputation(:votes, :all, order: 'votes desc')
+    # @features = Feature.group_by_day(:created_at).count
     # @features = Feature.order("DATE(start_at)").group("DATE(start_at)").count
     # @features_by_date = Feature.group_by_day(:created_at).reverse_order.count
 
