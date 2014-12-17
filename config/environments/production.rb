9b017a146bb1d56e5da905418a3a652f4154e5d8
Jeremy Vandehey <jgvandehey@gmail.com>
2014-12-17 13:14:16 -0800

    retweak s3 config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,9 +68,9 @@ config.i18n.fallbacks = true
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => 'S3_BUCKET_NAME',
-      :access_key_id => 'AWS_ACCESS_KEY_ID',
-      :secret_access_key => 'AWS_SECRET_ACCESS_KEY'
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
